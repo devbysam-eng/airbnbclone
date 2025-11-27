5ad1ad035d27f256b85c597cfcdd69bbb5ebfb51
devbysam-eng <devbysamir@gmail.com>
2025-11-27 14:44:40 +0545

    Register Modal complete

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -2,8 +2,8 @@
 import { IconType } from "react-icons";
 
 interface ButtonProps {
-  label?: string;
-  onClick: () => void;
+  label: string;
+  onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
   disabled?: boolean;
   outline?: boolean;
   small?: boolean;
@@ -26,10 +26,10 @@ const Button: React.FC<ButtonProps> = ({
         ${outline ? "bg-white" : "bg-rose-500"} 
         ${outline ? "border-black" : "border-rose-500"} 
         ${outline ? "text-black" : "text-white"} 
-        ${small ? "text-white py-1" : "text-white py-3"}
+        ${small ? "py-1" : "py-3"}
         ${small ? "text-sm" : "text-md"}
         ${small ? "font-light" : "font-semibold"}
-        ${small ? "border-[1px]" : "border-2"}`}
+        ${small ? "border" : "border-2"}`}
     >
       {Icon && <Icon size={24} className="absolute left-4 top-3" />}
       {label}
